c665b5085c84f2b41d443171e76de1640672a8d3
Bhaawna Manoranjan <bhaawnamanoranjan@192-168-1-114.tpgi.com.au>
2022-05-30 12:20:14 +1000

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # addressCheck
-To validate the address
+To validate the address via www.belong.com and https://www.belong.com.au/go/internet.
+
+TO run the test just navigate to Cypress > integration > belong-test and run the below command
+
+`npx cypress open`
--- cypress/integration/belong-test/check-address.js
+++ cypress/integration/belong-test/check-address.js
@@ -1,15 +1,29 @@
 ///<reference types= "Cypress"/>
 
-describe('Belong Coding Test', () =>{
-    it('To validate Check address', ()=>{
-        cy.visit('https://www.belong.com.au/go/internet/check-your-address');
+describe('Belong Coding Test', () => {
+
+    it('TO validate address check via belong.com', () => {
+        cy.visit('https://www.belong.com.au/');
+        cy.get(':nth-child(3) > [data-testid="action-tile__root"] > .ActionTilestyles__ContentWrapper-y8oq8z-5 > [data-testid="action-tile__button"] > .Buttonstyles__ContentContainer-sc-170nuae-1').click();
+        cy.wait(6000);
+        cy.get('[data-testid="primary-button"] > .Buttonstyles__ContentContainer-sc-170nuae-1').click();
+        cy.wait(6000);
         cy.get('[data-testid="step-address-lookup__input"]').should('be.visible');
         cy.get('[data-testid="step-address-lookup__input"]').type('31 Delias');
         cy.contains('31 Delias Road').click();
         cy.waitFor(6000);
         cy.contains('Review and confirm your address');
         cy.get('[data-testid="step-address-lookup__submit-button"]').click();
-        cy.wait(8000);
+        cy.contains('Great! We can connect you to NBN internet');
+    });
+
+    it('To validate Check address via Internet', ()=>{
+        cy.visit('https://www.belong.com.au/go/internet/check-your-address');
+        cy.get('[data-testid="step-address-lookup__input"]').should('be.visible');
+        cy.get('[data-testid="step-address-lookup__input"]').type('31 Delias');
+        cy.contains('31 Delias Road').click();
+        cy.contains('Review and confirm your address');
+        cy.get('[data-testid="step-address-lookup__submit-button"]').click();
         cy.contains('Great! We can connect you to NBN internet');
     })
 })
